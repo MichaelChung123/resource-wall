5dfa53880e2e50cc7c991bb724dc07fa8d0f6abc
Michael Chung <michael.chung1994@gmail.com>
2018-12-22 01:42:41 +0000

    working on getting resources into footer without user routes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,23 @@
 $(() => {
   $.ajax({
     method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+    url: "/api/resources"
+  }).done((resources) => {
+    for(post of resources) {
+      $("<div>").text(post.title).appendTo($("body"));
+      $("<div>").text(post.description).appendTo($("body"));
+      $("<div>").text(post.url).appendTo($("body"));
+      // $("<div>").text(post.title).appendTo($("body"));
     }
   });;
 });
+
+// function getAllPosts(){
+//   return knex.select("*").from("resources")
+//   .then((result) => {
+//       console.log(result);
+//   })
+// }
+
+// getAllPosts();
+
