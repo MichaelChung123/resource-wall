1dd0ec43f8ac953832b365fb6a54846342d9560b
Negar Amiri <negamiri@gmail.com>
2018-12-22 17:07:37 +0000

    Add all routes

--- routes/collectiondetails.js
+++ routes/collectiondetails.js
@@ -8,7 +8,9 @@ module.exports = (knex) => {
   router.get("/", (req, res) => {
     knex
       .select("*")
-      .from("users")
+      .from("collection_details")
+      .join('collections', 'collections.id')
+      .join('resources', 'resources.id')
       .then((results) => {
         res.json(results);
     });
--- server.js
+++ server.js
@@ -16,6 +16,10 @@ const knexLogger  = require('knex-logger');
 
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
+const resourcesRoutes = require("./routes/resources");
+const collectionsRoutes = require("./routes/collections");
+const collectiondetailsRoutes = require("./routes/collectiondetails");
+
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -37,6 +41,9 @@ app.use(express.static("public"));
 
 // Mount all resource routes
 app.use("/api/users", usersRoutes(knex));
+app.use("/api/resources", resourcesRoutes(knex));
+app.use("/api/collections", collectionsRoutes(knex));
+app.use("/api/collectiondetails", collectiondetailsRoutes(knex));
 
 // Home page
 app.get("/", (req, res) => {
