326f4ae1ad57b36caf99e8df05908cae94c38971
Negar Amiri <negamiri@gmail.com>
2018-12-29 20:30:13 +0000

    Change user collection logic to be able to search for collection names with spaces

--- routes/userscollection.js
+++ routes/userscollection.js
@@ -7,13 +7,15 @@ module.exports = (knex) => {
 
   router.get("/", (req, res) => {
     const referer = req.headers.referer.split('/');
+    const collectionName = referer[4].split("%20").join(' ');
+    console.log("Collection name:", collectionName);
     knex
       .select("*")
       .from("collection_details")
       .join('resources', 'collection_details.resource_id', 'resources.id')
       .join('collections', 'collection_details.collection_id', 'collections.id')
       .join('users', 'collections.user_id', 'users.id')
-      .where('collections.name', referer[4])
+      .where('collections.name', collectionName)
       .where('users.username', referer[3])
       .then((results) => {
         res.json(results);
