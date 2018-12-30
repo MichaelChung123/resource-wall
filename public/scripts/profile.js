1bb8854e910215d79fb52e6e57eea32b58b3f71f
Michael Chung <michael.chung1994@gmail.com>
2018-12-30 03:35:26 +0000

    adding multiple collections and posts to the profile page

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -3,35 +3,65 @@ $(() => {
     method: "GET",
     url: "/api/profile"
   }).done((users) => {
-    createResourceElement(users);
-    console.log(users[1].username);
+    renderResources(users);
+    // createResourceElement(users);
+    // createProfileContent(users)
+    console.log(users[0]);
   });
 });
 
 function createResourceElement(users) {
-  let id = users.id;
-  let name = users.name;
-  let username = users.username;
-  let password = users.password;
-  let photo = users.photo;
+  let id = users[0].id;
+  let name = users[0].name;
+  let username = users[0].username;
+  let password = users[0].password;
+  let photo = users[0].photo;
 
   let $users = $("header").html(`
     <div class='row'>
-    <div class='col-sm-4'>
-    <label>${username}</label>
+      <div class='col-sm-4'>
         <img src="../images/bobross.jpeg" class="img-rounded user-avatar" />
-    </div>
-    <div class='col-sm-3'>
-        <h1 class='user-profile'>Bob Ross</h1>
-    </div>
+      </div>
+      <div class='col-sm-3'>
+        <h1 class='user-profile'>${username}</h1>
+      </div>
     </div>
         `);
   return $users[0];
 
 }
 
+function createProfileContent(users) {
+  let name = users.name;
+  let username = users.username;
+  let password = users.password;
+  let photo = users.photo;
+  let collectionName = users.name;
+  let topic = users.topic;
+
+  let $content = $('article').html(`
+    <div class= 'row content-row'>
+    <div class='col-sm-offset-1 col-sm-2 collection1'>
+      <label>${collectionName}</label>
+      <div class='row'>
+        <label>${topic}</label>
+      </div>
+      <div class='row'>
+        <label>${username}</label>
+      </div>
+    </div>
+    `)
+
+    return $content[0];
+}
+
+
 function renderResources(usersArray) {
   $('.body-container').empty();
 
-  $('.body-container').append(createResourceElement(usersArray[1]));
+  $('.body-container').append(createResourceElement(usersArray));
+
+  for(let x of usersArray) {
+    $('.body-container').append(createProfileContent(x));
+  }
 }
--- routes/profile-data.js
+++ routes/profile-data.js
@@ -1,21 +1,30 @@
 "use strict";
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (knex) => {
 
   router.get("/", (req, res) => {
     const referer = req.headers.referer.split('/');
+    console.log(req.headers);
     knex
       .select("*")
       .from("collection_details")
       .join('resources', 'collection_details.resource_id', 'resources.id')
-      .join('collections', 'collection_details.collection_id', 'collections.id')
       .join('users', 'collection_details.id', 'users.id')
+      .join('collections', 'collection_details.collection_id', 'collections.id')
+      .where('users.username', referer[3])
       .then((results) => {
         res.json(results);
-    });
+      });
+
+    // knex("likes")
+    //   .where('resource_id', referer[3])
+    //   .count('resource_id')
+    //   .then((results) => {
+    //     res.json(results);
+    //   });
   });
 
   return router;
