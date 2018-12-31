86c7b3c8ed49f8a294005c6d4d11d223306f4910
Negar Amiri <negamiri@gmail.com>
2018-12-31 06:15:09 +0000

    Fix reference to resource on collection page

--- public/scripts/userscollection.js
+++ public/scripts/userscollection.js
@@ -8,7 +8,7 @@ $(() => {
         const $header = $("<header>").appendTo($resource);
         $("<h2>").text(details.title).appendTo($header);
         $("<p>").text(details.description).appendTo($resource);
-        const $footer = $("<footer>").html(`<a href="/${details.resource_id}">View resource</a>`).appendTo($resource);
+        const $footer = $("<footer>").html(`<a href="/${details.id}">View resource</a>`).appendTo($resource);
         $('<i class="icon fas fa-star"></i>').appendTo($footer);
         $('<i class="icon fas fa-comment"></i>').appendTo($footer);
         $('<i class="icon fas fa-heart"></i>').appendTo($footer);
