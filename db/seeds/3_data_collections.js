b0423f6eabd19c719fdc2a6c167f9d9cb8bb1417
Negar Amiri <negamiri@gmail.com>
2019-01-01 01:35:31 +0000

    Fix seeds

--- db/seeds/3_data_collections.js
+++ db/seeds/3_data_collections.js
@@ -5,26 +5,32 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         knex('collections').insert({
+          id: '1',
           user_id: 1, 
           name: 'Coding'
         }),
         knex('collections').insert({
+          id: '2',
           user_id: 2, 
           name: 'Lighthouse Resources'
         }),
         knex('collections').insert({
+          id: '3',
           user_id: 3, 
           name: 'Javascript'
         }),
         knex('collections').insert({
+          id: '4',
           user_id: 1, 
           name: 'Coding fundamentals'
         }),
         knex('collections').insert({
+          id: '5',
           user_id: 2, 
           name: 'Web development'
         }),
         knex('collections').insert({
+          id: '6',
           user_id: 3, 
           name: 'My resources'
         })
